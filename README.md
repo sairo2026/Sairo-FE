3a316556eac129c2664580e39d98ad7ea83f16f5
little stone <83966093+KimTaeHwan21@users.noreply.github.com>
2026-08-10 23:06:24 +0900

    chore: 프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sairo FE
+
+사이로 프론트엔드 레포입니다.
+
+## 스택
+- Next.js (App Router)
+- TypeScript
+- Tailwind CSS
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>사이로</h1>
+    </main>
+  );
+}
